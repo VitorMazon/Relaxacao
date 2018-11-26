7ab2889f5d7b834b2651ec2b2ec1c38cc0d59ac7
VitorMazon <vitormazon@id.uff.br>
2018-11-26 09:21:59 +0000

    Profile da relaxacao

--- Ex1/script.sh
+++ Ex1/script.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-for i in $(seq 1 5)
-do 
-  gcc -O2 -lm relax.c -o rl
-  ./rl 1e-9 > sol.dat
-  gnuplot map.gp
-  tempo=$(grep 'tempo = ' sol.dat | cut -d = -f 2)
-  echo $i '  ' $tempo >> temp9.dat
+for j in 0 1 2 3
+do
+  for i in $(seq 1 5)
+  do 
+    gcc -O$j -lm relax.c -o rl
+    ./rl 1e-9 > sol.dat
+    gnuplot map.gp
+    tempo=$(grep 'tempo = ' sol.dat | cut -d = -f 2)
+    echo $i '  ' $tempo >> temp9.dat
+  done
 done
